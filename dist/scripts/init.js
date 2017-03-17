460ad8c0cbfa658cbd6e3f7ddd5d859cdf8eb8f5
DmitrySolo <solo-webworks@mail.ru>
2017-03-17 19:02:08 +0300

    Pre Alpha 0.0.698

--- dist/scripts/init.js
+++ dist/scripts/init.js
@@ -205,19 +205,22 @@ $(document).ready(function () {
 });
 //TO CART
 
-$('.toCart').on('click',function () {
+$('.toCart').on('click', function () {
 
     var ajaxResp = true;
 
-    if (ajaxResp){
-        var _this =$(this)
-        _this.find('use').attr('xlink:href','#loader').delay(5);
-
+    if (ajaxResp) {
+        var _this = $(this)
+        _this.find('use').attr('xlink:href', '#loader').delay(5);
 
 
         setTimeout(function () {
-            _this.addClass('toCart--in').prop('disabled',true);
-            _this.find('use').attr('xlink:href','#check');
-        },2000)
+            $('.ove-cartCounter--phantom').addClass('animate').delay(1000).queue(function (next) {
+                $(this).removeClass("animate");
+                next();
+            });
+            _this.addClass('toCart--in').prop('disabled', true);
+            _this.find('use').attr('xlink:href', '#check');
+        }, 2000)
     }
 })
