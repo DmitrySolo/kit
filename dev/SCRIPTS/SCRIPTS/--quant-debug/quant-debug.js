bb2f45438edf4013c2c8bb3fc7ee46613de213c2
DmitrySolo <solo-webworks@mail.ru>
2017-05-25 19:05:40 +0300

    17523

--- dev/SCRIPTS/SCRIPTS/--quant-debug/quant-debug.js
+++ dev/SCRIPTS/SCRIPTS/--quant-debug/quant-debug.js
@@ -3,7 +3,22 @@
 $( document ).ready(function() {
 var engP = 'Platy bonito oceanic whitetip shark orangespine unicorn fish loach goby rockweed gunnel turkeyfish Port Jackson shark buffalofish, southern grayling. Arapaima viperfish eeltail catfish pearl danio Black swallower, Atlantic trout sailfin silverside. Tang, marlin tui chub Indian mul flashlight fish, skilfish loosejaw lenok porcupinefish bandfish. Clownfish eeltail catfish: freshwater hatchetfish codlet tenpounder ladyfish scissor-tail rasbora lancetfish tigerperch king of herring. Grideye Mozambique tilapia oceanic whitetip shark clingfish North American darter mail-cheeked fish lamprey bramble shark. Parrotfish loweye catfish squaretail. Lighthousefish yellowhead jawfish shark mola mola sunfish.';
 var eHead = 'This is Header';
-$('a').removeAttr('href');
+
+var addToBufer= function (content) {
+    let tmp   = document.createElement('INPUT'), // Создаём новый текстовой input
+        focus = document.activeElement; // Получаем ссылку на элемент в фокусе (чтобы не терять фокус)
+    tmp.value =content;
+
+    // Временному input вставляем текст для копирования
+    document.body.appendChild(tmp); // Вставляем input в DOM
+    tmp.select(); // Выделяем весь текст в input
+    document.execCommand('copy'); // Магия! Копирует в буфер выделенный текст (см. команду выше)
+    document.body.removeChild(tmp); // Удаляем временный input
+    focus.focus();
+
+}
+
+    $('a').removeAttr('href');
 //qntDragDrop( document.getElementById('ball'));
     $('#ball').draggable();
     $('.spacer').resizable();
@@ -35,16 +50,21 @@ $('a').removeAttr('href');
             _this.css("lineHeight",_this.height()+'px');
             _this.resizable( "destroy" );
         }}).draggable();
-        let tmp   = document.createElement('INPUT'), // Создаём новый текстовой input
-            focus = document.activeElement; // Получаем ссылку на элемент в фокусе (чтобы не терять фокус)
+
+
+
 
         var Classes = $(this).attr('class').replace(/resizeble/,'')
             .replace(/debugElement/g,'')
             .replace(/ui-resizable/g,'')
             .replace(/ui-draggable-handle/g,'')
             .replace(/ui-draggable/g,'');
+
         var tag = $(this).prop("tagName");
+
         newnotifyStr = tag+'>'+Classes;
+
+        //$(this).append("<span style='position: absolute;top:-20px; left:0;font-size:10px;'>"+newnotifyStr+"</span>")
         if (newnotifyStr != notifyStr)
         {$(this).notify(newnotifyStr,{autoHideDelay: 2000,style:'tagClassInfo'});
             notifyStr =newnotifyStr}
@@ -55,13 +75,9 @@ $('a').removeAttr('href');
             if (buferMsgArr[clName] != '')
                 buferMsg+='.'+buferMsgArr[clName]+' ';
         }
+        addToBufer(buferMsg);
 
-        tmp.value =buferMsg;  // Временному input вставляем текст для копирования
-        document.body.appendChild(tmp); // Вставляем input в DOM
-        tmp.select(); // Выделяем весь текст в input
-        document.execCommand('copy'); // Магия! Копирует в буфер выделенный текст (см. команду выше)
-        document.body.removeChild(tmp); // Удаляем временный input
-        focus.focus(); // Возвращаем фокус туда, где был
+         // Возвращаем фокус туда, где был
         fontSize = $(this).css('fontSize');
         $('#ball').css('fontSize',fontSize);
         $('.lh').text('Line height of font-size ='+ fontSize);
@@ -99,14 +115,49 @@ $('a').removeAttr('href');
         MakeEditable($('.mod.header'));
     })
     $('#addP').on('click',function () {
-        $(".debugElement").append( "<p class='mod prgph'></p>" );
+        $(".debugElement").append( "<p class='mod prgph'>"+engP+"</p>" );
         MakeEditable($('.mod.prgph'));
     })
     $('#clone').on('click',function () {
-        $( ".debugElement" ).clone().appendTo( "body" ).css({"position":"absolute","top":"0"}).removeClass("debugElement")
+        var clone = $( ".debugElement" ).clone().appendTo( "body" ).css({"position":"absolute","top":"0"}).removeClass("debugElement")
             .draggable()
             .resizable();
+        MakeEditable($(clone));
 
+
+    })
+
+    $('#del').on('click',function () {
+        $(".debugElement").remove();
+    })
+    $('#100p').on('click',function () {
+        $(".debugElement").css('width','100%');
+    })
+
+    $('#getHtml').on('click',function () {
+
+        $('.notifyjs-wrapper').remove();
+        $('.ui-resizable-handle').remove();
+
+        var topug = $(".debugElement").parent().html()
+      // $(".notifyjs-wrapper",$(".debugElement").parent()).remove();
+        topug = topug.replace(/resizeble/g,'')
+                .replace(/eventLock/g,'')
+                .replace(/ui-resizable/g,'')
+                .replace(/ui-resizable-handle/g,'')
+                .replace(/ui-draggable/g,'')
+                .replace(/ui-draggable-handle/g,'')
+                .replace(/debugElement/g,'');
+       addToBufer(topug );
+
+       if (document.getElementById('htmlToPugctn') == null){
+           $('body').append('<div id="htmlToPugctn" class="htmlToPug"><iframe id="pugIfrm" src="http://jumplink.github.io/jade2html2jade/" width="400" height="700" align="left"></iframe><span class="js-trigger">close</span></div>');
+       }
+
+
+       //Html2Jade.convertDocument(document, {}, function (err, jade) {
+            // do your thing
+        //});
 
     })
     $('#down').on('click',function () {
@@ -124,6 +175,42 @@ $('a').removeAttr('href');
 
 
     })
+    $('#lock').on('click',function () {
+        $( ".debugElement" ).toggleClass('eventLock');
+
+
+    })
+    $('#unlock').on('click',function () {
+        $( ".eventLock" ).removeClass("eventLock");
+
+
+    })
+    $('#htmlToPugctn .js-trigger').on('click',function () {
+        $('#htmlToPugctn').remove();
+
+
+    })
+    $('#showAllTags--').on('click',function () {
+
+        $('*').each(function () {
+            MakeEditable($(this));
+            if ($( this ).attr('class')){
+                console.log('hello');
+            var Classes = $( this ).attr('class').replace(/resizeble/,'')
+                .replace(/debugElement/g,'')
+                .replace(/ui-resizable/g,'')
+                .replace(/ui-draggable-handle/g,'')
+                .replace(/ui-draggable/g,'');
+
+            var tag = $( this ).prop("tagName");
+
+            var newnotifyStr = tag+'>'+Classes;
+            $( this ).prepend("<div style='position: relative';><span class='tagChecker' style='background:"+$(this).css('background')+"'>"+newnotifyStr+"</span></div>")
+
+        }})
+
+    })
+
 
 
 });
