6485047accb97dd6ba9e9f510bbdec9faa9542e0
DmitrySolo <solo-webworks@mail.ru>
2017-11-24 19:26:40 +0300

    blur test

--- Projects/stsFragments/dev/qContent/MODULES/productInline/productInline.js
+++ Projects/stsFragments/dev/qContent/MODULES/productInline/productInline.js
@@ -2,6 +2,13 @@
 * MODULE: productInline script
 *
 */
+$(document).ready(function () {
+    var photo = document.getElementById("opa"); 
+    TweenLite.to(photo, 1.5, {x:100});
+})
+
+
+
 var quantitySelectorArr = document.getElementsByClassName('productInline__quantitySelector');
 for(var i=0;i<quantitySelectorArr.length;i++){
     quantitySelectorArr[i].addEventListener('focus',function () {
--- Projects/stsFragments/dist/scripts/init.js
+++ Projects/stsFragments/dist/scripts/init.js
@@ -34,12 +34,20 @@ $('.toOrder.active').on('mousedown',function () {
 * MODULE: productInline script
 *
 */
+$(document).ready(function () {
+    var photo = document.getElementById("opa"); 
+    TweenLite.to(photo, 1.5, {x:100});
+})
+
+
+
 var quantitySelectorArr = document.getElementsByClassName('productInline__quantitySelector');
 for(var i=0;i<quantitySelectorArr.length;i++){
     quantitySelectorArr[i].addEventListener('focus',function () {
         this.select();
     },false)
 }
+
 // Element: b1 script.
 
 // Element: dashBoardElement script.
