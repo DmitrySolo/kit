cf38e67f5ce35c5083b8e1146e94b4f427b650d4
DmitrySolo <solo-webworks@mail.ru>
2017-12-10 17:48:43 +0300

    blur test

--- Projects/stsFragments/dev/qContent/MODULES/catalogList/catalogList.js
+++ Projects/stsFragments/dev/qContent/MODULES/catalogList/catalogList.js
@@ -1,3 +1,45 @@
 /**
 * MODULE: catalogList script
 */
+
+(function(){
+	var
+	selectorIdPrefix = 'js_showSub',
+	selectorsArr = document.querySelectorAll('.catalogList__categoryItem'),
+	targetsArr = document.querySelectorAll('.catalogList__SublistItem'),
+	lastTarget = false,
+	target,
+	gogo;   
+
+ 
+	for(var i = 0; i<=selectorsArr.length; i++ ){
+        
+		selectorsArr[i].addEventListener('mouseenter',function (e) {
+			var _this = this;
+			   
+		    gogo = setTimeout(function(){
+
+				    var showNum = _this.getAttribute('id').split('__')[1];
+				    target = document.getElementById('js_sub__'+showNum);
+					
+					if(target){
+						if(lastTarget){
+							lastTarget.style.display = 'none';
+							}
+							target.style.display = 'block';
+							lastTarget = target;
+							
+						}
+
+
+		}, 300);
+
+   
+
+		})
+
+		selectorsArr[i].addEventListener('mouseleave',function (e) {
+			clearTimeout(gogo);
+		})
+	}
+}())
