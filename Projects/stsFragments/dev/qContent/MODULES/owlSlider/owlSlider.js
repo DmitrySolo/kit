c5ce02a36143f66a99e3c62331a11115b80fb59e
DmitrySolo <solo-webworks@mail.ru>
2017-12-17 17:04:33 +0300

    blur test

---
+++ Projects/stsFragments/dev/qContent/MODULES/owlSlider/owlSlider.js
@@ -0,0 +1,10 @@
+//owlSlider Script
+$(document).ready(function(){
+    $(".ove-mainSlider").owlCarousel(
+        {
+            "items":45,
+            "lazyLoad":true,
+            "autoplay":true
+        }
+    );
+});
